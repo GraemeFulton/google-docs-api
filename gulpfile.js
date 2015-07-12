499e4274ea3af0ebd281f99a1e71c2d12fc089f9
Graeme Fulton <graemefulton89@gmail.com>
2015-07-12 18:41:00 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,6 @@
-//'use strict';
-//
-//var gulp = require('gulp');
-//
-//gulp.paths = {
-//  src: 'src',
-//  dist: 'dist',
-//  tmp: '.tmp',
-//  e2e: 'e2e'
-//};
-//
-//require('require-dir')('./gulp');
-//
-//gulp.task('default', ['clean'], function () {
-//    gulp.start('build');
-//});
-
 'use strict';
 
 var gulp = require('gulp');
-var del = require('del');
-var push = require('git-push');
-var argv = require('minimist')(process.argv.slice(2));
 
 gulp.paths = {
   src: 'src',
@@ -33,12 +13,4 @@ require('require-dir')('./gulp');
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
-});
-
-
-gulp.task('deploy', function(cb) {
-  var remote = argv.production ?
-    {name: 'production', url: 'https://github.com/<org>/site.com', branch: 'gh-pages'},
-    {name: 'test', url: 'https://github.com/<org>/test.site.com', branch: 'gh-pages'};
-  push('./build', remote, cb);
 });
