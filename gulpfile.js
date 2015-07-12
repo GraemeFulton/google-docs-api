6d1c8e47d7b11db1d8ebdc3cb753ec7915b122a1
Graeme Fulton <graemefulton89@gmail.com>
2015-07-12 18:36:35 +0100

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
+//'use strict';
+//
+//var gulp = require('gulp');
+//
+//gulp.paths = {
+//  src: 'src',
+//  dist: 'dist',
+//  tmp: '.tmp',
+//  e2e: 'e2e'
+//};
+//
+//require('require-dir')('./gulp');
+//
+//gulp.task('default', ['clean'], function () {
+//    gulp.start('build');
+//});
+
 'use strict';
 
 var gulp = require('gulp');
+var del = require('del');
+var push = require('git-push');
+var argv = require('minimist')(process.argv.slice(2));
 
 gulp.paths = {
   src: 'src',
@@ -15,6 +35,10 @@ gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
 
-gulp.task("heroku:production", function(){
-    console.log('hello'); // the task does not need to do anything.
+
+gulp.task('deploy', function(cb) {
+  var remote = argv.production ?
+    {name: 'production', url: 'https://github.com/<org>/site.com', branch: 'gh-pages'},
+    {name: 'test', url: 'https://github.com/<org>/test.site.com', branch: 'gh-pages'};
+  push('./build', remote, cb);
 });
